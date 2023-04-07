b594047a4d03cad856fa032c0e06d3dac1382da0
wocaishiniliu <2982733101@qq.com>
2023-04-07 11:05:29 +0800

    Update logger.py

--- experiments/Climate/gru_ode_bayes/logger.py
+++ experiments/Climate/gru_ode_bayes/logger.py
@@ -16,8 +16,11 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        #self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
 
     def save_dict(self,info,epoch):
         """
